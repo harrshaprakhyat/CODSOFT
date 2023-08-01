a2c5c93c8d0019b2d24c871bb3773cf3506ac05e
harrshaprakhyat <133838051+harrshaprakhyat@users.noreply.github.com>
2023-08-01 22:17:29 +0530

    Add files via upload

--- Task3_password/password.py
+++ Task3_password/password.py
@@ -1,1 +1,26 @@
+import random
+import string
 
+def generate_password(length):
+    # Define the characters to be used in the password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Generate a random password using the specified length
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    try:
+        # Prompt the user to specify the desired length of the password
+        desired_length = int(input("Enter the desired length of the password: "))
+
+        # Generate the password
+        password = generate_password(desired_length)
+
+        # Display the generated password
+        print("Generated Password:", password)
+    except ValueError:
+        print("Invalid input. Please enter a valid integer for the password length.")
+
+if __name__ == "__main__":
+    main()
